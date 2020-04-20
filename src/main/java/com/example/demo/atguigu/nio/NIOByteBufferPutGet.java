e642fd362ea4a51ee75e78257492f86ef1ad6c03
martea <1642485710@qq.com>
2020-04-20 10:34:19 +0800

    NioByteBuffer

---
+++ src/main/java/com/example/demo/atguigu/nio/NIOByteBufferPutGet.java
@@ -0,0 +1,36 @@
+package com.example.demo.atguigu.nio;
+
+import java.nio.ByteBuffer;
+
+/**
+ * 使用nio的ByteBuffer
+ */
+public class NIOByteBufferPutGet {
+    public static void main(String[] args) {
+
+        //创建一个Buffer
+        ByteBuffer buffer = ByteBuffer.allocate(64);
+
+        //类型化方式放入数据
+        buffer.putInt(100);
+        buffer.putLong(9);
+        buffer.putChar('尚');
+        buffer.putShort((short) 4);
+
+        //取出
+        buffer.flip();
+
+        System.out.println();
+
+        System.out.println(buffer.getInt());
+        System.out.println(buffer.getLong());
+        System.out.println(buffer.getChar());
+        System.out.println(buffer.getShort());
+
+
+
+
+
+
+    }
+}
